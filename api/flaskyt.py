c184a0a37585977ce8093f9b5125d27767a18a71
klingersf <60621086+klingersf@users.noreply.github.com>
2021-01-25 00:30:42 -0300

    Update flaskyt.py

--- api/flaskyt.py
+++ api/flaskyt.py
@@ -5,10 +5,14 @@ import json
 import random
 import uuid
 import time
-from flask import Flask, Response
+from flask import Flask, Response, request
 app = Flask(__name__)
+
+
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+    id = request.args.get('id')
+    x = request.args.get('x')
+    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (idx), mimetype="text/html")
