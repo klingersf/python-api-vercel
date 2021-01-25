ebd97d7795a8bce4ba8ab47ddb5d9525f117e598
klingersf <60621086+klingersf@users.noreply.github.com>
2021-01-24 22:42:15 -0300

    Update star.py

--- api/star.py
+++ api/star.py
@@ -27,9 +27,9 @@ class handler(BaseHTTPRequestHandler):
     result = json.dumps(
         {
             "id": str(uuid.uuid4()),
-            "選號日期": time.strftime("%m/%d/%Y", time.localtime()) ,
-            "可能中獎號碼": random.sample(range(10),3),
-            "猜中機率": 0
+            "Data": time.strftime("%m/%d/%Y", time.localtime()) ,
+            "Numbers": random.sample(range(10),3),
+            "Probalilidade": 0
         }, ensure_ascii=False)
     self.send_response(200)
     self.send_header('Content-type', 'application/json')
