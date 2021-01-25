fe5feee7475f7f01abf3882ec069370265af5192
klingersf <60621086+klingersf@users.noreply.github.com>
2021-01-25 01:45:59 -0300

    Update combo.py

--- api/combo.py
+++ api/combo.py
@@ -14,14 +14,14 @@ app = Flask(__name__);
 @app.route('/<path:path>')
 def catch_all(path):
     id = request.args.get('id')
-    x = request.args.get('x')
+    lang = request.args.get('x')
     transcricao = YouTubeTranscriptApi.get_transcript('A8zyhKRebus', languages=['pt'])
     result = json.dumps(
         {
             "id": str(uuid.uuid4()),
             "Data": time.strftime("%m/%d/%Y", time.localtime()) ,
             "Numbers": random.sample(range(10),3),
-            "videoURL": idVideo,
+            "videoURL": id,
             "idioma": lang,
             "transcription": transcricao
         }, ensure_ascii=False)
